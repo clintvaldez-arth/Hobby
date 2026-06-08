6c78b2f4d8c5ec6ec1044bcfbc6aee26c4fd35b1
gregbangyay-aws <greg.bangyay@awsys-i.com>
2026-06-08 15:20:26 +0800

    Added Greg Bangyay's Hobbies

--- Hobby.java
+++ Hobby.java
@@ -4,4 +4,12 @@ displayTraineeHobbyA();
 displayTraineeHobbyB();
 displayTraineeHobbyC();
 }
+
+public static void displayTraineeHobbyX() {
+System.out.println("Trainee X’s Hobbies:");
+System.out.println("1.) Gaming");
+System.out.println("2.) Anime");
+System.out.println("3.) Manga");
+System.out.println("4.) Yes");
+}
 }
